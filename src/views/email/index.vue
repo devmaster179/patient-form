9655f893ff97771edaa358ad1f680270cd27e633
alextd79 <alextd.dev@gmail.com>
2022-11-27 11:27:00 +0530

    email form added

--- src/views/email/index.vue
+++ src/views/email/index.vue
@@ -1,21 +1,40 @@
 <template>
+  <v-container fluid class="emailWrapper">
+    <v-row>
+      <v-col cols="12" md="12">
+        <div class="d-flex logoWrap">
+          <a href="#"><img src="../../assets/clearLogo.png" /></a>
+        </div>
+      </v-col>
+    </v-row>
+    <div class="emailForm">
     <v-form v-model="valid">
-      <v-container fluid class="blueColor">
-        <v-row>
-          <v-col cols="12" md="8">
-            <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
-              :counter="10"
-              label="Search"
-              required
-              placeholder="Try typing your hospital / clinic name or address"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-row class="align-items-center">
+      <v-col cols="12" md="3">
+      <v-btn class="emailArrowBtn" prepend-icon="mdi-arrow-left"> </v-btn>
+      </v-col>
+        <v-col cols="12" md="6">
+        <div class="emailFormInner">
+        <h2>What's your email address ?</h2>
+        <label>Email</label>
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="e.g. example@gmail.com"
+            required
+            placeholder="e.g. example@gmail.com"
+          ></v-text-field>
+            <div class="pressEnter">
+             Press Enter <v-btn class="pressEnterBtn" prepend-icon="mdi-arrow-right"> </v-btn>
+            </div>
+          </div>
+        </v-col>
+          <v-col cols="12" md="3"></v-col>
+      </v-row>
     </v-form>
-
+    </div>
+  </v-container>
 </template>
 
 <script>
