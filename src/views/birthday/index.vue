dc39fc0922bb01cb4ac02e496de3adc5e196d8b6
alextd79 <alextd.dev@gmail.com>
2022-11-27 14:49:52 +0530

    birthday added

--- src/views/birthday/index.vue
+++ src/views/birthday/index.vue
@@ -1,21 +1,57 @@
 <template>
+  <v-container fluid class="emailWrapper">
+    <v-row>
+      <v-col cols="12" md="12">
+        <div class="d-flex logoWrap">
+          <a href="#"><img src="../../assets/clearLogo.png" /></a>
+        </div>
+      </v-col>
+    </v-row>
+    <div class="emailForm">
     <v-form v-model="valid">
-      <v-container fluid class="blueColor">
-        <v-row>
-          <v-col cols="12" md="8">
-            <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
-              :counter="10"
-              label="Search"
-              required
-              placeholder="Try typing your hospital / clinic name or address"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-row class="align-items-center">
+      <v-col cols="12" md="3">
+      <v-btn class="emailArrowBtn" prepend-icon="mdi-arrow-left"> </v-btn>
+      </v-col>
+        <v-col cols="12" md="6">
+        <div class="emailFormInner">
+        <h2>What's your date of birth ?</h2>
+        <p>As it appears on your health card.</p>
+        <label>Date of Birth</label>
+        <div class="birthdayFields">
+        <v-select
+  label="Month"
+  :items="['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']"
+></v-select>
+          <v-text-field
+          class="yearField"
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="DD"
+            required
+            placeholder="DD"
+          ></v-text-field>
+          <v-text-field
+          class="yearField"
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="YYYY"
+            required
+            placeholder="YYYY"
+          ></v-text-field>
+          </div>
+            <div class="pressEnter">
+             Press Enter <v-btn class="pressEnterBtn" prepend-icon="mdi-arrow-right"> </v-btn>
+            </div>
+          </div>
+        </v-col>
+          <v-col cols="12" md="3"></v-col>
+      </v-row>
     </v-form>
-
+    </div>
+  </v-container>
 </template>
 
 <script>
--- src/views/email/index.vue
+++ src/views/email/index.vue
@@ -8,31 +8,33 @@
       </v-col>
     </v-row>
     <div class="emailForm">
-    <v-form v-model="valid">
-      <v-row class="align-items-center">
-      <v-col cols="12" md="3">
-      <v-btn class="emailArrowBtn" prepend-icon="mdi-arrow-left"> </v-btn>
-      </v-col>
-        <v-col cols="12" md="6">
-        <div class="emailFormInner">
-        <h2>What's your email address ?</h2>
-        <label>Email</label>
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="e.g. example@gmail.com"
-            required
-            placeholder="e.g. example@gmail.com"
-          ></v-text-field>
-            <div class="pressEnter">
-             Press Enter <v-btn class="pressEnterBtn" prepend-icon="mdi-arrow-right"> </v-btn>
+      <v-form v-model="valid">
+        <v-row class="align-items-center">
+          <v-col cols="12" md="3">
+            <v-btn class="emailArrowBtn" prepend-icon="mdi-arrow-left"> </v-btn>
+          </v-col>
+          <v-col cols="12" md="6">
+            <div class="emailFormInner">
+              <h2>What's your email address ?</h2>
+              <label>Email</label>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                :counter="10"
+                label="e.g. example@gmail.com"
+                required
+                placeholder="e.g. example@gmail.com"
+              ></v-text-field>
+              <div class="pressEnter">
+                Press Enter
+                <v-btn class="pressEnterBtn" prepend-icon="mdi-arrow-right" @click="next()">
+                </v-btn>
+              </div>
             </div>
-          </div>
-        </v-col>
+          </v-col>
           <v-col cols="12" md="3"></v-col>
-      </v-row>
-    </v-form>
+        </v-row>
+      </v-form>
     </div>
   </v-container>
 </template>
@@ -42,18 +44,18 @@ import "./style.css";
 
 export default {
   data: () => ({
-    valid: false,
-    firstname: "",
-    lastname: "",
-    nameRules: [
-      (v) => !!v || "Name is required",
-      (v) => v.length <= 10 || "Name must be less than 10 characters",
-    ],
     email: "",
     emailRules: [
       (v) => !!v || "E-mail is required",
       (v) => /.+@.+/.test(v) || "E-mail must be valid",
     ],
   }),
+  mounted() {},
+  methods: {
+    next() {
+      if(this.email)
+      this.$router.push({ name: "Birthday" });
+    },
+  },
 };
 </script>
