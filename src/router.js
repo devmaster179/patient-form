b7bf132b1ad07aab2c6bf5566d27b9b63e78d536
alextd79 <alextd.dev@gmail.com>
2022-11-28 08:22:38 +0200

    signup

--- src/router.js
+++ src/router.js
@@ -1,12 +1,8 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Search from "@/views/search";
-import Imaging from "./views/imaging";
-import Email from "./views/email";
-import Birthday from "./views/birthday";
-import Name from "./views/name";
-import Health from "./views/health";
-import Phone from "./views/phone";
-import Confirm from "./views/confirm";
+import Form from "./views/form";
+import Signup from "./views/signup";
+import Imaging from './views/imaging';
 
 const routes = [
   {
@@ -15,39 +11,19 @@ const routes = [
     component: Search,
   },
   {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
     path: "/imaging",
     name: "Imaging",
     component: Imaging,
   },
   {
-    path: "/email",
-    name: "Email",
-    component: Email,
-  },
-  {
-    path: "/birthday",
-    name: "Birthday",
-    component: Birthday,
-  },
-  {
-    path: "/name",
-    name: "Name",
-    component: Name,
-  },
-  {
-    path: "/health",
-    name: "Health",
-    component: Health,
-  },
-  {
-    path: "/phone",
-    name: "Phone",
-    component: Phone,
-  },
-  {
-    path: "/confirm",
-    name: "Confirm",
-    component: Confirm,
+    path: "/form",
+    name: "Form",
+    component: Form,
   },
 ];
 
---
+++ src/views/signup/index.vue
@@ -0,0 +1,138 @@
+<template>
+  <v-container fluid class="signupWrapper">
+    <v-row>
+      <v-col cols="12" md="12">
+        <div class="d-flex logoWrapper">
+          <img src="../../assets/nexusLogo.jpg" />
+          <h1>Signup Patient</h1>
+        </div>
+      </v-col>
+    </v-row>
+
+    <div class="">
+      <v-form v-model="valid">
+        <v-row class="align-items-center">
+          <v-col cols="12" md="2"></v-col>
+          <v-col cols="12" md="8">
+            <div class="signupInner">
+              <div class="formGroup">
+                <label>Email address*</label>
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  required
+                ></v-text-field>
+              </div>
+              <v-row class="align-items-center">
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>First name*</label>
+                    <v-text-field
+                      v-model="text"
+                      :rules="textRules"
+                      required
+                    ></v-text-field>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>Last name*</label>
+                    <v-text-field
+                      v-model="text"
+                      :rules="textRules"
+                      required
+                    ></v-text-field>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row class="align-items-center">
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>Password*</label>
+                    <v-text-field
+                      v-model="text"
+                      :rules="textRules"
+                      required
+                    ></v-text-field>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>Confirm Password*</label>
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      required
+                    ></v-text-field>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row class="align-items-center">
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>Country Code*</label>
+                    <v-select
+                      label="Select"
+                      :items="['United States(1)', 'India(91)']"
+                    ></v-select>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="6">
+                  <div class="formGroup">
+                    <label>Phone Number*</label>
+                    <v-text-field
+                      v-model="password"
+                      :rules="passwordRules"
+                      required
+                    ></v-text-field>
+                  </div>
+                </v-col>
+              </v-row>
+              <div class="formGroup">
+                <label>Date of Birth</label>
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  required
+                ></v-text-field>
+              </div>
+
+              <div class="formGroup">
+                <v-checkbox
+                  v-model="terms"
+                  color="secondary"
+                  label="I agree to the terms of service and Privacy Policy*"
+                ></v-checkbox>
+              </div>
+              <div class="submitBtn">
+                <v-btn block> Submit </v-btn>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="12" md="2"></v-col>
+        </v-row>
+      </v-form>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import "./style.css";
+
+export default {
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+};
+</script>
